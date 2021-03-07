f21e9ae4612b0abb4e1a33464e222dd4131b3878
Joshua Laughner <jlaugh@caltech.edu>
2021-03-06 22:39:58 -0800

    Switched from command line inputs to action inputs

--- index.js
+++ index.js
@@ -3,19 +3,23 @@ const toml = require('toml');
 const fs = require('fs');
 
 try {
-    const release_tag = process.argv[2];
-    //const release_tag = core.getInput('release-tag');
+    // Might make more sense to get the release tag directly from the github
+    // API (if possible) than to have it as an input.
+    const release_tag = core.getInput('release-tag');
+    const cargo_toml_path = core.getInput('cargo-toml-path');
     
-    const data = fs.readFileSync('Cargo.toml', 'utf-8');//, function(err, data) {
+    const data = fs.readFileSync(cargo_toml_path, 'utf-8');
     cargo_config = toml.parse(data);
     crate_version = cargo_config.package.version;
     
     const result = check_tag(release_tag, crate_version);
         
     if (result) {
-        console.log('Matches!');
+        console.log('Release tag is consistent with the crate version!');
     }else{
-        console.log('No match :(');
+        const msg = 'Release tag is NOT consistent with the crate version :('
+        console.log(msg);
+        core.setFailed(msg);
     }
     
 } catch (error) {
@@ -24,7 +28,7 @@ try {
 }
 
 function check_tag(release_tag, crate_version) {
-    const check_mode = process.argv[3];
+    const check_mode = core.getInput('check-mode');
     if (check_mode == 'strict') {
         return check_strict(release_tag, crate_version);
     }else if (check_mode == 'simple') {
@@ -40,8 +44,8 @@ function check_simple(release_tag, crate_version) {
 }
 
 function check_strict(release_tag, crate_version) {
-    const prefix = process.argv[4];
-    const suffix = process.argv[5];
+    const prefix = core.getInput('prefix');
+    const suffix = core.getInput('suffix');
     const release_regex = new RegExp(`${prefix}${crate_version}${suffix}`);
     console.log(`Checking if release tag (${release_tag}) matches the regex ${release_regex}`);
     return release_regex.test(release_tag);
